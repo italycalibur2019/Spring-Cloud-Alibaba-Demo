a32b03e96484eedcb1ef2d8af025be40d2d2bb3e
dhr <920893925@qq.com>
2025-02-09 16:09:59 +0800

    注册网关服务

---
+++ Spring-Cloud-Alibaba-Demo-Gateway/src/test/java/com/italycalibur/ciallo/gateway/GatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.italycalibur.ciallo.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class GatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
