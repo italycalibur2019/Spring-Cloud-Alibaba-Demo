4ba3b591362a802528c90a13243f3ecbeded57b4
dhr <dhr@cqkgkj.com>
2025-02-20 10:37:04 +0800

    全局过滤器日志打印优化

---
+++ Spring-Cloud-Alibaba-Demo-Common/src/main/java/com/italycalibur/ciallo/common/enums/GatewayServiceEnums.java
@@ -0,0 +1,34 @@
+package com.italycalibur.ciallo.common.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+/**
+ * @description: 模块枚举类
+ * @author dhr
+ * @date 2025-02-20 10:14:03
+ * @version 1.0
+ */
+@Getter
+@AllArgsConstructor
+public enum GatewayServiceEnums {
+    ADMIN_SERVICE("admin-service", "管理模块"),
+    BASIC_SERVICE("basic-service", "基础模块"),
+    ORDER_SERVICE("order-service", "订单模块"),
+    STOCK_SERVICE("stock-service", "库存模块"),
+    FEE_SERVICE("fee-service", "费用模块"),
+    AUTH_SERVICE("auth-service", "认证模块"),
+    UNKNOWN("unknown", "未知服务");
+
+    private final String code;
+    private final String desc;
+
+    public static String getDescByCode(String routeId) {
+        for (GatewayServiceEnums value : GatewayServiceEnums.values()) {
+            if (value.getCode().equals(routeId)) {
+                return value.getDesc();
+            }
+        }
+        return UNKNOWN.getDesc();
+    }
+}
--- Spring-Cloud-Alibaba-Demo-Gateway/src/main/java/com/italycalibur/ciallo/gateway/filter/AuthGlobalFilter.java
+++ Spring-Cloud-Alibaba-Demo-Gateway/src/main/java/com/italycalibur/ciallo/gateway/filter/AuthGlobalFilter.java
@@ -1,10 +1,14 @@
 package com.italycalibur.ciallo.gateway.filter;
 
+import com.italycalibur.ciallo.common.enums.GatewayServiceEnums;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.gateway.filter.GatewayFilterChain;
 import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.cloud.gateway.route.Route;
+import org.springframework.cloud.gateway.support.ServerWebExchangeUtils;
 import org.springframework.core.Ordered;
 import org.springframework.stereotype.Component;
+import org.springframework.util.ObjectUtils;
 import org.springframework.web.server.ServerWebExchange;
 import reactor.core.publisher.Mono;
 
@@ -19,7 +23,12 @@ import reactor.core.publisher.Mono;
 public class AuthGlobalFilter implements GlobalFilter, Ordered {
     @Override
     public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
-        log.info("====== (●'◡'●) 过滤器正常工作~ 当前访问路径：{} ======", exchange.getRequest().getURI().getPath());
+        // 获得路由
+        Route route = exchange.getAttribute(ServerWebExchangeUtils.GATEWAY_ROUTE_ATTR);
+        // 获得路由id
+        String routeId = ObjectUtils.isEmpty(route) ? "unknown" : route.getId();
+        // 打印日志
+        log.info("====== (●'◡'●) 过滤器正常工作~ 当前访问服务：{}，访问路径：{} ======", GatewayServiceEnums.getDescByCode(routeId), exchange.getRequest().getURI().getPath());
         return chain.filter(exchange);
     }
 
